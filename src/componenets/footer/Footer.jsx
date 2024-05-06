c12a2e1e6ea94aed58b7db77ee135ef7f42374f0
debdutta04 <debdutta0401@gmail.com>
2024-05-06 23:11:34 +0530

    code added

---
+++ src/componenets/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                 Explore the cinematic universe with <b>MOVIX</b>,crafted by <b>Debdutta</b>.MOVIX offers a seamless experience to search for movies and TV shows,
+                  providing current ratings and official trailers. Delve into detailed information such as director, 
+                  writer, release date, and runtime. Discover top cast members with their profile images and names, 
+                  all in one place. 
+                  Experience the world of entertainment like never before with MOVIX
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
